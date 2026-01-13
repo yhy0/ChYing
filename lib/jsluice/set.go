0609682e2c190c9211431f4aa5d440dece1d3d0f
yhy0 <31311038+yhy0@users.noreply.github.com>
2026-01-13 18:16:17 +0800

    Revert "fix: 修复 Windows 平台 jsluice 构建错误"
    This reverts commit df4a4a3624dee7ea8c767bfd9553a878d67055d6.

--- lib/jsluice/set.go
+++ lib/jsluice/set.go
@@ -1,5 +1,3 @@
-//go:build !windows
-
 package jsluice
 
 type set map[string]any
--- pkg/Jie/scan/gadget/collection/jsluice_windows.go
+++
@@ -1,9 +0,0 @@
-//go:build windows
-
-package collection
-
-// analyzeJsluice is a stub for Windows platform where tree-sitter-javascript is not available.
-// It returns empty results on Windows.
-func analyzeJsluice(target, body string) []string {
-	return []string{}
-}
